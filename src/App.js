ab39ef83be70b1ec35db47140461132fd1c31587
Gustavo Oli <69ggustavoo69@gmail.com>
2024-05-14 11:30:13 -0300

    update tela

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Body } from './componets/Body';
 function App() {
   return (
     <div className="App">
       <header />
-    
+      <Body></Body>
     
 
     </div>
--- src/componets/Body.js
+++ src/componets/Body.js
@@ -7,7 +7,7 @@ export function Body() {
 
     return (
         <div className='body_Main'>
-            <div className='3D_body_Main'>
+            <div className='d3_body_Main'>
                 <div className='3D_body'>
                 
                 </div>
