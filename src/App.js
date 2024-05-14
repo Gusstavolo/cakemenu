0f1f6993e90f79455f3cf9a90ae1fd5be778b3da
Gustavo Oli <69ggustavoo69@gmail.com>
2024-05-13 22:13:44 -0300

    update header

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
-        
-      </header>
+      <header />
+    
+    
+
     </div>
   );
 }
---
+++ src/componets/Body.js
@@ -0,0 +1,22 @@
+import '../App.css';
+
+
+export function Body() {
+
+
+
+    return (
+        <div className='body_Main'>
+            <div className='3D_body_Main'>
+                <div className='3D_body'>
+                
+                </div>
+            </div>
+
+
+
+
+        </div>
+
+    )
+}
